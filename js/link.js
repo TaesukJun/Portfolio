7d9ea79ae5a62a13dc879b54fc01f908e651660b
TaesukJun <taesuk92@gmail.com>
2024-03-26 03:47:29 +0900

    stable version for html project

---
+++ js/link.js
@@ -0,0 +1,14 @@
+
+
+let linkG = document.getElementById("linkGallery");
+linkG.href = 'gallery.html';
+
+
+document.getElementById("linkAboutMe").href = "gallery.html";
+document.getElementById("linkContact").href = "mailto:tjun@purdue.edu";
+document.getElementById("linkWebDevelopment").href = "#";
+document.getElementById("linkDcmViewer").href = "https://github.com/TaesukJun/DCM_Viewer";
+document.getElementById("linkGraphicDesign").href = "#";
+document.getElementById("linkPravacy").href = "#";
+document.getElementById("linkDisclaimer").href = "#";
+document.getElementById("linkGooglePlay").href = "#";
