b534eb0fee0ca643f2f04b78c54d397aef88a7f2
TaesukJun <taesuk92@gmail.com>
2024-03-27 03:31:54 +0900

    hrml css portofolio update

---
+++ js/link-temp.js
@@ -0,0 +1,35 @@
+
+gallerySite = 'gallery.html'
+
+
+document.querySelector(".header-gallery").setAttribute('href',gallerySite);
+document.querySelector(".burger-gallery").setAttribute('href',gallerySite);
+document.querySelector(".footer-main-sitemap-01-gallery").setAttribute('href',gallerySite);
+document.querySelector(".footer-main-sitemap-02-gallery").setAttribute('href',gallerySite);
+
+
+aboutMeSite = '#';
+
+document.querySelector(".header-aboutMe").setAttribute('href',aboutMeSite);
+document.querySelector(".burger-aboutMe").setAttribute('href',aboutMeSite);
+document.querySelector(".footer-main-sitemap-01-aboutMe").setAttribute('href',aboutMeSite);
+//document.querySelector(".main-intro-aboutMe").setAttribute('href',aboutMeSite);
+
+
+contacturl = "mailto:tjun@purdue.edu";
+
+document.querySelector(".header-contact").setAttribute('href',contacturl);
+document.querySelector(".burger-contact").setAttribute('href',contacturl);
+document.querySelector(".footer-main-sitemap-01-contact").setAttribute('href',contacturl);
+
+
+WDSite = '#'; //Web Development
+document.querySelector(".footer-main-sitemap-02-WD").setAttribute('href',WDSite);
+
+
+DVSite = "https://github.com/TaesukJun/DCM_Viewer"; //DCM viwer
+document.querySelector(".footer-main-sitemap-02-DV").setAttribute('href',DVSite);
+
+
+GDSite = "#"; //DCM viwer
+document.querySelector(".footer-main-sitemap-02-GD").setAttribute('href',GDSite);
