3996dda879cf0e9bf478d0212896dcb938016720
TaesukJun <taesuk92@gmail.com>
2024-04-22 23:44:55 +0900

    practice

---
+++ test.py
@@ -0,0 +1,24 @@
+print('hello world')
+
+## TODO: productively check
+for i in range(100):
+
+
+    print(i)
+
+
+
+for i in range(100):
+    for j in range(100):
+        
+        
+        
+        print(i,j)
+
+
+
+## FIXME: Hello World
+## BUG: This is a bug
+## NOTE: 
+## HELPME: 
+## REVIEW:
